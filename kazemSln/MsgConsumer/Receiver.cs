c29f70793a83ed0f157f84c9f7bcc73272ebeeca
mrjm.khavari@gmail.com <mrjm.khavari@gmail.com>
2021-08-17 11:12:06 +0300

    simple consumer producer test

---
+++ kazemSln/MsgConsumer/Receiver.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace MsgConsumer
+{
+    public class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var chanel=connection.CreateModel())
+                {
+                    chanel.QueueDeclare("BasicTestQ", false, false, false, null);
+                    var consumer = new EventingBasicConsumer(chanel);
+                    // consumer.Received += Consumer_Received;
+
+
+                    consumer.Received += (model, e) =>
+                     {
+                         var body = e.Body.Span;
+                         var msg = Encoding.UTF8.GetString(body);
+                         Console.WriteLine("recived =>  {0}", msg);
+                     };
+                   chanel.BasicConsume("BasicTestQ", true, consumer);
+
+
+                }
+                
+            }
+            Console.WriteLine("finish");
+            Console.ReadLine();
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            // throw new NotImplementedException();
+            var body = e.Body;
+            var msg = Encoding.UTF8.GetString(body.ToArray());
+            Console.WriteLine("recived =>  {0}", msg);
+        }
+    }
+}
---
+++ kazemSln/MsgProducer/Sender.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace MsgProducer
+{
+    class Sender
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection =factory.CreateConnection())
+                using(var chanel=connection.CreateModel())
+            {
+                chanel.QueueDeclare("BasicTestQ", false, false, false, null);
+                string msg = "This is the Msg";
+                var body = Encoding.UTF8.GetBytes(msg);
+                chanel.BasicPublish("", "BasicTestQ", null, body);
+
+                Console.WriteLine("Message Sent");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
